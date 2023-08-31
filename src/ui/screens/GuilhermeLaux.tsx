1e07026035fa66d7e01dbd753da1052db6697730
Guilherme Laux <lauxguilherme@hotmail.com>
2023-08-30 23:58:57 -0300

    feat/wip

--- src/ui/screens/GuilhermeLaux.tsx
+++ src/ui/screens/GuilhermeLaux.tsx
@@ -1,9 +1,11 @@
 import AboutMe from "@components/about-me/AboutMe";
 import styles from "./GuilhermeLaux.module.css";
+import Header from "@components/header/Header";
 
 export default function GuilhermeLaux() {
   return (
     <div className={styles.guilherme__laux}>
+      <Header />
       <AboutMe />
       <h1>kkkkkkkkkkkkkk</h1>
     </div>
